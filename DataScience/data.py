72b7e26adca37fe87e6b51fb9a554738c1b6f1c5
tuan trinh <88110221+tuan882612@users.noreply.github.com>
2022-06-07 09:57:44 -0500

    Update data.py

--- DataScience/data.py
+++ DataScience/data.py
@@ -5,7 +5,7 @@ def get_metaData(data):
     print(data['table']['column_header'])
 
 def parse(data):
-    arr = ['birth_date', 'birth_place', 'name', 'death_date','occupation','profession']
+    arr = ['birth_date', 'birth_place', 'name', 'death_date','occupation']
     res = []
     hash = {}
     
@@ -23,19 +23,57 @@ def parse(data):
     return hash
     
 def parse_age(birth, death):
-    print(int(death.split()[-1])-int(birth.split()[-1]))
+    arr = []
+    arr2 = []
+    
+    for i in birth.split():
+    
+        if len(i) == 4 and i[0].isnumeric():
+            arr2.append(i)
+            
+    for i in death.split():
+    
+        if len(i) == 4 and i[0].isnumeric():
+            arr.append(i)
+    
+    if len(arr) == 0 or len(arr2) == 0:
+        return
+    
+    else:
+        if len(arr) > 1:
+            for i in range(len(arr)):
+                arr[i] = int(arr[i])
+                
+            death = str(max(arr))
+            
+        else:
+            death = str(arr[0])
+            
+        if len(arr2) > 1:
+            for i in range(len(arr2)):
+                arr[i] = int(arr2[i])
+                
+            birth = str(max(arr2))
+            
+        else:
+            birth = str(arr2[0])
+
+        print(str(int(death)-int(birth)))
 
 def get_input_text():
     data = load_dataset("wiki_bio") #['input_text', 'target_text']
-
-    for i in zip(data['test']['input_text'][:],
-                 data['test']['target_text'][:]):
+    count = 0
+    
+    for i in zip(data['train']['input_text'][:],
+                 data['train']['target_text'][:]):
         temp = parse(i[0])
         
         if len(temp) >= 5:
-            age = parse_age(temp['birth_date'],temp['death_date'])
+            # print(temp['birth_date'],temp['death_date'])
+            # age = parse_age(temp['birth_date'],temp['death_date'])
             # temp['target_text'] = i[1]
-            # print(temp)
+            if 'occupation' in temp:
+                print(temp['occupation'])
             
     # with open('data.json', 'w', encoding='utf-8') as f:
         
